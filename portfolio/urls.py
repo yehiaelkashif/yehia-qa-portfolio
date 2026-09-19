35752610c15b67c99da37aeb59c0b7a53428ff5c
yehiaelkashif <58746490+yehiaelkashif@users.noreply.github.com>
2026-09-19 15:33:25 +0300

    Add CV download URL

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
-from .views import home
-urlpatterns=[path('',home,name='home')]
+from .views import home, download_cv
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("download-cv/", download_cv, name="download_cv"),
+]
